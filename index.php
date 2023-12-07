70837b933d88583ead9fa417596ebb855d010d8e
alAsif <saikatt.iub@gmail.com>
2023-12-08 04:10:47 +0600

    up

---
+++ index.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome to IUB 360</title>
+  <link rel="icon" href="https://seeklogo.com/images/I/independent-university-logo-776F5F3A69-seeklogo.com.png">
+
+  <script src="https://cdn.tailwindcss.com"></script>
+
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js" defer></script>
+
+</head>
+
+<body>
+  <?php include('assets/header.php') ?>
+
+  <section class="bg-center bg-no-repeat bg-blue-900 pt-24">
+    <div class="px-4 mx-auto max-w-screen-xl text-center pb-6 lg:pt-6">
+      <h1 class="mb-4 text-3xl font-extrabold text-white sm:text-4xl">
+        Entrepreneurs are the visionaries who sculpt the future with their ideas and drive change with their
+        determination.</h1>
+      <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Student startup provide students
+        with hands-on experience, foster enufidsgtrepreneurship, and can lead to the development of successful businesses
+        beyond their academic careers.</p>
+      <a href="#" class="inline-flex justify-center items-center py-3 px-5  mb-5 text-base font-medium  bg-white text-blue-900 rounded-lg border border-blue-900 hover:bg-blue-100 hover:text-blue-900">
+        Get Started
+      </a>
+    </div>
+  </section>
+
+  <section class="bg-white">
+    <div class="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-12 lg:py-16">
+      <div class="max-w-2xl mx-auto text-center mb-12">
+        <h2 class="text-3xl font-extrabold text-blue-900 sm:text-4xl">
+          We Stand For
+        </h2>
+        <p class="mt-4 text-base font-bold text-blue-900 sm:text-xl">
+          Crafted with skill and care to help our students grow their business!
+        </p>
+      </div>
+
+      <div class="grid grid-cols-1 mt-12 text-center sm:mt-20 gap-x-20 gap-y-20 sm:grid-cols-2 lg:grid-cols-3 space-x-4">
+        <div class="space-y-4">
+
+          <h3 class="text-2xl font-extrabold  text-blue-900 ">
+            Empowerment
+          </h3>
+          <img class="rounded-lg" src="./img/pexels-photo-5833767.jpeg" alt="stock">
+          <p class="text-lg font-bold text-blue-900">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ipsum repellat magnam vel architecto,
+            blanditiis impedit, in ut consequatur nam, sint vero alias iste repudiandae beatae exercitationem iure!
+            Possimus, assumenda?
+          </p>
+
+        </div>
+
+        <div class="space-y-4">
+
+          <h3 class="text-2xl font-extrabold text-blue-900">
+            Growth
+          </h3>
+          <img class="rounded-lg" src="./img/pexels-photo-5833767.jpeg" alt="stock">
+          <p class="text-lg font-bold text-blue-900">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci provident enim aut facilis, dolorem
+            possimus sit voluptas minus, dignissimos veritatis quia. Obcaecati aut quae eos delectus ducimus numquam
+            voluptas aperiam!
+          </p>
+
+        </div>
+
+        <div class="space-y-4">
+
+          <h3 class="text-2xl font-extrabold text-blue-900">
+            Coummunity
+          </h3>
+          <img class="rounded-lg" src="./img/pexels-photo-5833767.jpeg" alt="stock">
+          <p class="text-lg font-bold text-blue-900">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio saepe soluta illo omnis totam nostrum facere
+            deleniti repudiandae amet aliquam fugiat, praesentium nesciunt impedit optio quisquam, sint minima fugit
+            eos.
+          </p>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="flex flex-row justify-center">
+
+      <a href="#"
+        class="inline-flex justify-center items-center py-3 px-5  mb-5 text-base font-medium bg-blue-900 text-white rounded-lg border border-blue-900 hover:bg-blue-100 hover:text-blue-900 hover:border-blue-100">
+        Learn more
+      </a>
+    </div>
+  </section>
+
+
+  <!-- section: What makes IUB 360 special -->
+
+  <section class="flex max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-12 lg:py-16">
+    <div class="pr-10">
+      <h1 class="text-5xl text-blue-900 font-bold basis-1/5 mb-12">What makes IUB 360 special?</h1>
+
+      <div class="flex justify-around">
+        <div>
+          <div class="flex justify-center font-bold text-3xl mb-4">17</div>
+          <div class="font-bold text-2xl text-white rounded-full bg-blue-900 flex items-center justify-center text-center" style="height: 150px; width: 150px;">Total <br> Project</div>
+        </div>
+        <div>
+          <div class="flex justify-center font-bold text-3xl mb-4">05</div>
+          <div class="font-bold text-2xl text-white rounded-full bg-blue-900 flex items-center justify-center text-center" style="height: 150px; width: 150px;">Star <br> Project</div>
+        </div>
+        <div>
+          <div class="flex justify-center font-bold text-3xl mb-4">97%</div>
+          <div class="font-bold text-2xl text-white rounded-full bg-blue-900 flex items-center justify-center text-center" style="height: 150px; width: 150px;">Success <br> Rate</div>
+        </div>
+      </div>
+    </div>
+
+    <!-- <hr class="flex flex-row"> -->
+
+    <div class="space-y-8 lg:grid lg:grid-cols-2 sm:gap-6 xl:gap-10 lg:space-y-0 basis-4/5">
+      <!-- Pricing Card -->
+      <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-gray-100 rounded-lg border border-gray-100 shadow xl:p-8">
+        <h3 class="mb-4 text-2xl font-semibold">Sprint Program</h3>
+        <p class="text-gray-900 sm:text-lg">for</p>
+        <div class="flex justify-center items-baseline my-8">
+          <span class="mr-2 text-5xl font-extrabold">6</span>
+          <span class="text-gray-900">months</span>
+        </div>
+        <!-- List -->
+        <ul role="list" class="list-disc list-inside mb-8 space-y-4 text-left">
+          <li class="items-center space-x-3">
+            <!-- Icon -->
+            <span>1st level: <span class="font-semibold">Idea Cultivator (2 months)</span></span>
+          </li>
+          <li class="items-center space-x-3">
+            <!-- Icon -->
+            <span>2nd level: <span class="font-semibold">Incubator (4months)</span></span>
+          </li>
+          <li class="opacity-0 items-center space-x-3">
+            <!-- Icon -->
+            <span>- <span class="font-semibold">-</span></span>
+          </li>
+        </ul>
+        <form>
+          <!-- <label class="block mb-2 text-sm font-medium text-gray-900" for="file_input">Upload report</label>
+                    <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" id="file_input" type="file" accept=".pdf" required> -->
+          <button type="submit" class="mt-4 text-white bg-blue-900 border border-blue-900 hover:text-blue-900 hover:bg-blue-100 hover:border-blue-100 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Apply</button>
+          <button type="submit" class="mt-4 text-white bg-blue-900 border border-blue-900 hover:text-blue-900 hover:bg-blue-100 hover:border-blue-100 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">How it Works</button>
+        </form>
+      </div>
+
+      <!-- Pricing Card -->
+      <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-gray-100 rounded-lg border border-gray-100 shadow xl:p-8">
+        <h3 class="mb-4 text-2xl font-semibold">Full Program</h3>
+        <p class="text-gray-900 sm:text-lg">for</p>
+        <div class="flex justify-center items-baseline my-8">
+          <span class="mr-2 text-5xl font-extrabold">12</span>
+          <span class="text-gray-900">months</span>
+        </div>
+        <!-- List -->
+        <ul role="list" class="list-disc list-inside mb-8 space-y-4 text-left">
+          <li class="items-center space-x-3">
+            <!-- Icon -->
+            <span>1st level: <span class="font-semibold">Idea Cultivator (3 months)</span></span>
+          </li>
+          <li class="items-center space-x-3">
+            <!-- Icon -->
+            <span>2nd level: <span class="font-semibold">Incubator (6 months)</span></span>
+          </li>
+          <li class="items-center space-x-3">
+            <!-- Icon -->
+            <span>3rd level: <span class="font-semibold">Accelarator (3 months)</span></span>
+          </li>
+        </ul>
+        <form>
+          <!-- <label class="block mb-2 text-sm font-medium text-gray-900" for="file_input">Upload report</label>
+                    <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" accept=".pdf" required> -->
+          <button type="submit" class="mt-4 text-white bg-blue-900 border border-blue-900 hover:text-blue-900 hover:bg-blue-100 hover:border-blue-100 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Apply</button>
+          <button type="submit" class="mt-4 text-white bg-blue-900 border border-blue-900 hover:text-blue-900 hover:bg-blue-100 hover:border-blue-100 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center">How it Works</button>
+        </form>
+      </div>
+
+    </div>
+  </section>
+
+
+  <!-- section: Carousel -->
+
+  <section class="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-12 lg:py-16">
+    <div class="max-w-2xl mx-auto text-center mb-12">
+      <h1 class="text-3xl font-extrabold text-blue-900 sm:text-4xl">
+        Star Alumni
+      </h1>
+      <p class="mt-4 text-base font-bold text-blue-900 sm:text-xl">
+        Details!
+      </p>
+    </div>
+
+
+    <div id="default-carousel" class="relative w-full" data-carousel="slide">
+      <!-- Carousel wrapper -->
+      <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+        <!-- Item 1 -->
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img src="/img/image1.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        </div>
+        <!-- Item 2 -->
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img src="/img/image2.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        </div>
+        <!-- Item 3 -->
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img src="/img/image3.jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+        </div>
+      </div>
+      <!-- Slider indicators -->
+      <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+      </div>
+      <!-- Slider controls -->
+      <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+          <svg class="w-4 h-4 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+          </svg>
+          <span class="sr-only">Previous</span>
+        </span>
+      </button>
+      <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+          <svg class="w-4 h-4 text-white rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+          </svg>
+          <span class="sr-only">Next</span>
+        </span>
+      </button>
+    </div>
+
+  </section>
+
+
+  <!-- section: What’s happening at IUB 360 -->
+
+  <section class="flex justify-between max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-12 lg:py-16">
+    <div class="pr-10">
+      <h1 class="text-5xl text-blue-900 font-bold basis-1/5 mb-12">What's happening at IUB 360?!</h1>
+
+      <div class="flex">
+        <div class="flex flex-col">
+            <div class="font-bold text-3xl mb-4">Keep up with the latest news from IUB 360!</div>
+            <button type="submit" class="mt-4 text-white bg-blue-900 border border-blue-900 hover:text-blue-900 hover:bg-blue-100 hover:border-blue-100 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5">Event Calendar</button>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="space-y-8 lg:grid lg:grid-cols-2 sm:gap-6 xl:gap-10 lg:space-y-0 basis-4/5">
+
+      <!-- Image Card -->
+      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+        <a href="#">
+          <img class="rounded-t-lg" src="/img/image4.jpg" alt="" />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+            Read more
+            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+          </a>
+        </div>
+      </div>
+
+
+      <div>
+        <!-- Horizontal Card -->
+        <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+          <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/img/image4.jpg" alt="">
+          <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
+            <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </div>
+        </a>
+
+        <!-- Horizontal Card -->
+        <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+          <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/img/image4.jpg" alt="">
+          <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
+            <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </div>
+        </a>
+
+        <!-- Horizontal Card -->
+        <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+          <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/img/image4.jpg" alt="">
+          <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
+            <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          </div>
+        </a>
+      </div>
+
+
+
+
+    </div>
+  </section>
+
+  <?php include('assets/footer.php') ?>
+
+</body>
+
+</html>
